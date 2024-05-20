6c6afd69f40d84af69e7b3a897de9fa450a1bd52
hectr212 <163376058+hectr212@users.noreply.github.com>
2024-05-19 18:31:43 -0600

    Add files via upload

---
+++ tarea/condicional.py
@@ -0,0 +1,23 @@
+x = 5
+if x < 20:
+    print("Smaller")
+
+if x > 20:
+    print("Bigger")
+
+print("Finnis")
+
+
+y = 5
+
+if (y == 5):
+    print("Es igual")
+
+elif (y < 5):
+    print("Es menor")
+
+else:
+    print("Es mayor")
+
+
+
---
+++ tarea/ejemplo1.py
@@ -0,0 +1,9 @@
+numero = int(input("Adivina el valor del dado (1 - 6): \n"))
+
+valorDado = 6
+
+if numero == valorDado:
+    print("Haz acertado, ganaste!")
+else:
+    print("No adivinaste, perdiste!")
+
